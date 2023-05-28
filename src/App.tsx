45b86646ade768ac9352eb590b39b4adfd08aa30
fawzibazari <fawzi.bazari2@gmail.com>
2023-05-28 23:10:23 +0200

    working on it

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ function inputFormater(startime:string) {
   const parced = startime.replace(':','')
   return `${parced}px`
 }
+function durationToHeight(duration:number) {
+  const hours = (duration / 60);
+  console.log(hours * 100);
+
+  return hours * 100
+}
   return (
     <div style={{ height: "2400px", width: "100%" }}>
       {input.map(list => (
@@ -13,11 +19,13 @@ function inputFormater(startime:string) {
         style={{
           backgroundColor: "red",
           position: "relative",
+          border: "1px solid",
           top: inputFormater(list.start),
+          height: durationToHeight(list.duration)
         }}
         key={list.id}
       >
-        {inputFormater(list.start)}
+        {list.id}
       </div> 
         ))}
     </div>
