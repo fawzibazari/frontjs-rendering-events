da90218686091ed7bfaf78c5173c5ce8bdc11a0d
fawzibazari <fawzi.bazari2@gmail.com>
2023-05-29 00:03:19 +0200

    i must find a way to create end_time

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,51 @@ import "./App.css";
 import input from "../input.json";
 
 function App() {
+  // function toHoursAndMinutes(totalMinutes: number) {
+  //   const hours = Math.floor(totalMinutes / 60);
+  //   const minutes = totalMinutes % 60;
+  //   return `${padToTwoDigits(hours)}:${padToTwoDigits(minutes)}`;
+  // }
+  // function padToTwoDigits(num: number) {
+  //   return num.toString().padStart(2, "0");
+  // }
 
+  // function endTime(start: string, duration: number) {
+
+  // }
+  // if inférieurer a end time du premier ils se chevauchent forcement
   return (
     <div style={{ height: "2400px", width: "100%" }}>
-      {input.map((list) => {
+      {input.map((list, index) => {
         const newObject = {
           start: list.start.replace(":", "").toString() + "px",
           height: (list.duration / 60) * 100,
+          // end
         };
+
         console.log(newObject);
 
-        return (
-          <div
-            style={{
-              backgroundColor: "red",
-              position: "relative",
-              border: "1px solid",
-              top: newObject.start,
-              height: newObject.height,
-            }}
-            key={list.id}
-          >
-            {list.id}
-          </div>
-        );
+        // console.log(index);
+        // if (list.start < list.end[index -1]) {
+
+        if (list.start) {
+          return (
+            <div
+              style={{
+                backgroundColor: "red",
+                position: "relative",
+                border: "1px solid",
+                top: newObject.start,
+                height: newObject.height,
+              }}
+              key={list.id}
+            >
+              {list.id}
+            </div>
+          );
+        } else {
+          return <p key={list.id}>hello</p>;
+        }
       })}
     </div>
   );
