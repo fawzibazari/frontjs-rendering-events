55bc0f92ab2a6b1ec3bd70c8e140c490e4987155
fawzibazari <fawzi.bazari2@gmail.com>
2023-05-28 23:42:35 +0200

    created an object

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,31 @@ import "./App.css";
 import input from "../input.json";
 
 function App() {
-function inputFormater(startime:string) {
-  const parced = startime.replace(':','')
-  return `${parced}px`
-}
-function durationToHeight(duration:number) {
-  const hours = (duration / 60);
-  console.log(hours * 100);
 
-  return hours * 100
-}
   return (
     <div style={{ height: "2400px", width: "100%" }}>
-      {input.map(list => (
-        <div
-        style={{
-          backgroundColor: "red",
-          position: "relative",
-          border: "1px solid",
-          top: inputFormater(list.start),
-          height: durationToHeight(list.duration)
-        }}
-        key={list.id}
-      >
-        {list.id}
-      </div> 
-        ))}
+      {input.map((list) => {
+        const newObject = {
+          start: list.start.replace(":", "").toString() + "px",
+          height: (list.duration / 60) * 100,
+        };
+        console.log(newObject);
+
+        return (
+          <div
+            style={{
+              backgroundColor: "red",
+              position: "relative",
+              border: "1px solid",
+              top: newObject.start,
+              height: newObject.height,
+            }}
+            key={list.id}
+          >
+            {list.id}
+          </div>
+        );
+      })}
     </div>
   );
 }
